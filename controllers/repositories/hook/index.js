6422b520020d99b2608844a436c457c7af075cc5
Leon Strauss <leon@ilu.st>
2016-11-04 16:51:49 +0100

    two fator auth with github works now

--- controllers/repositories/hook/index.js
+++ controllers/repositories/hook/index.js
@@ -11,36 +11,55 @@ router.route('/')
     .post(handlePost);
 
 function handlePost(req, res) {
-    console.log('Received push notification on repository ' + req.repository.name);
+    console.log('Received push notification on repository ' + req.repository._id);
     console.log('Cloning from ' + req.repository.url);
 
-    clone(req.repository).then(funciton(repo) {
+    clone(req.repository).then(function(repo) {
         return Promise.all(req.repository.remotes.map(function(remote) {
-            var fullRemoteName = req.repository.name + '/' + remote.name;
+            var fullRemoteName = req.repository._id + '/' + remote.name;
             console.log('Adding remote ' + fullRemoteName);
 
-            return Git.Remote.Create(repo, remote.name, remote.url).then(function(createdRemote) {
+            return Git.Remote.create(repo, remote.name, remote.url).then(function(createdRemote) {
                 console.log('Pushing to ' + fullRemoteName + ' ...');
 
                 //TODO: Branch support
-                return createdRemote.push('master').then(function() {
+                return createdRemote.push(
+                    ['refs/heads/master:refs/heads/master'],
+                    {
+                        callbacks: {
+                            certificateCheck: function() { return 1; },
+                            credentials: function(a,b) {
+                                return Git.Cred.userpassPlaintextNew('CODE_HERE', 'x-oauth-basic');
+                            }
+                        }
+                }).then(function() {
                     console.log('...push to ' + fullRemoteName + ' done');
+                }).catch(function (err) {
+                    console.error(err);
                 });
             });
         }));
     }).then(function() {
-        console.log('Hook for repository ' + req.repository.name + ' done!');
+        console.log('Hook for repository ' + req.repository._id + ' done!');
+    }).catch(function (err) {
+        console.error(err);
     });
 
     return res.status(204).send('');
 }
 
 function getTmpRepoPath(repo) {
-    return path.join(__dirname, 'tmpRepos');
+    return path.join(__dirname, 'tmpRepos/' + repo._id);
 }
 
 function clone(repo) {
-    return Git.Clone(repo.url, getTmpRepoPath(repo));
+    return Git.Clone(repo.url, getTmpRepoPath(repo), {
+        fetchOpts: {
+            callbacks: {
+                certificateCheck: function() { return 1; }
+            }
+        }
+    });
 }
 
 module.exports = router;
